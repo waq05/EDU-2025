444e63c604547888da4828bbab8fa53f89dba6d0
waq666 <qwe714509665@qq.com>
2025-06-23 11:29:29 +0800

    初始化

---
+++ src/main/java/com/EDU/Edu2025Application.java
@@ -0,0 +1,13 @@
+package com.EDU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edu2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Edu2025Application.class, args);
+    }
+
+}
